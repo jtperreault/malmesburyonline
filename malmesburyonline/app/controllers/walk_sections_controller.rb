df4e2ddea87837a9e6e6ca0bd8b3e9758e9df575
James T. Perreault <jperreault@terakeet.com>
2019-03-19 08:35:01 -0400

    Remove go_for method and views. If we want special naming in our visible route we should take care of that in the routes.rb file, and leave our naming of methods and files in line with standards.

--- malmesburyonline/app/controllers/walk_sections_controller.rb
+++ malmesburyonline/app/controllers/walk_sections_controller.rb
@@ -1,10 +1,4 @@
 class WalkSectionsController < ApplicationController
-  def go_for
-    @walk = Walk.first
-    @walk_section = WalkSection.find(params[:id])
-    @walk_section_buttons = @walk_section.walk_section_buttons
-  end
-
   def show
     # assuming we need to use the buttons in our view, we eager load them from the DB
     # using the includes() method.
