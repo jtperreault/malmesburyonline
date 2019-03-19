8465bbfd839ff9a36afdd3e139441a40efa5fb9c
James T. Perreault <jperreault@terakeet.com>
2019-03-19 08:32:57 -0400

    best guess for seeding the development DB with some data. missing buttons though!!

--- malmesburyonline/db/seeds.rb
+++ malmesburyonline/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+w = Walk.first_or_create!(name: "From Court House to St. John's", description: 'A nice walk from ye ole Court House to St. Johns')
+
+WalkSection.create!(name: 'Go This Way', image: 'IMG_8844.JPG', walk: w)
+WalkSection.create!(name: 'Than This Way', image: 'IMG_8845.JPG', walk: w)
+WalkSection.create!(name: 'Round Corner', image: 'IMG_8846.JPG', walk: w)
