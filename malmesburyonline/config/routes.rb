64aec79ff9de1a30e7c5e12dc15b9500d7a16cc9
James T. Perreault <jperreault@terakeet.com>
2019-03-19 08:32:31 -0400

    use resourceful style routing, limiting to index and shows

--- malmesburyonline/config/routes.rb
+++ malmesburyonline/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 #  get 'walk_section/go_for'
-  get 'walk_section/go_for/:id', to: 'walk_section#go_for'
+  #get 'walk_section/go_for/:id', to: 'walk_section#go_for'
+  resources :walks, only: [:index, :show] do
+    resources :walk_sections, only: [:show]
+  end
 #  get '/:id', to: 'walk_section#go_for'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
